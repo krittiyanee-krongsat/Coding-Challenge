f6a314ce2718bbcf05a60a98b925d8f1fd1b04d6
Krittiyanee Krongsat <krittiyanee.kron@gmail.com>
2026-09-08 15:37:30 +0700

    reset install backend

--- BE/.venv/Scripts/activate.bat
+++ BE/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\kritt\Downloads\Coding-Challenge\BE\.venv
+set VIRTUAL_ENV=D:\Work\Coding-Challenge\BE\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
